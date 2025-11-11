200b3f1b65af732b35567c225a4e9e4b6fdab8c0
Ansar Smagulov <ansar@anfragment.net>
2025-11-11 21:37:08 +0500

    filter: remove early true return on Sec-Fetch-Dest match
    Browsers send Sec-Fetch-Dest: document on requests for attachments.
    The result of isDocumentNavigation determines whether injectors run on the HTTP response body, which in turn caused encoding issues in downloaded assets.
    
    Related to ZenPrivacy/zen-desktop#534

--- filter/filter.go
+++ filter/filter.go
@@ -377,7 +377,9 @@ func isDocumentNavigation(req *http.Request, res *http.Response) bool {
 	// Note: Although not explicitly stated in the spec, Fetch Metadata Request Headers are only included in requests sent to HTTPS endpoints.
 	if req.URL.Scheme == "https" {
 		secFetchDest := req.Header.Get("Sec-Fetch-Dest")
-		return secFetchDest == "document" || secFetchDest == "iframe"
+		if secFetchDest != "document" && secFetchDest == "iframe" {
+			return false
+		}
 	}
 
 	contentType := res.Header.Get("Content-Type")
